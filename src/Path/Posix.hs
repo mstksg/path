3cb7395f5913c77b7a44ccae55a7e2df44861616
Justin Le <justin@jle.im>
2019-03-15 22:26:31 -0700

    Update gh-pages at Fri Mar 15 22:26:31 PDT 2019

--- src/Path/Posix.hs
+++
@@ -1,4 +0,0 @@
-{-# LANGUAGE CPP #-}
-#define PLATFORM_NAME   Posix
-#define IS_WINDOWS      False
-#include "Include.hs"
--- src/Path/Windows.hs
+++
@@ -1,4 +0,0 @@
-{-# LANGUAGE CPP #-}
-#define PLATFORM_NAME   Windows
-#define IS_WINDOWS      True
-#include "Include.hs"
